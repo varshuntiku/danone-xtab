cbeab865179d08ada3569471ea7eaa61e7b722c3
Varshun Tiku <varshun.tiku@themathcompany.com>
2025-01-30 16:31:30 +0530

    /docs path fix

--- docker/services/nuclios-server/api/main.py
+++ docker/services/nuclios-server/api/main.py
@@ -57,7 +57,7 @@ from api.routes import base_route
 @app.get(settings.DOCS_URL, include_in_schema=False)
 async def custom_swagger_ui_html():
     return get_swagger_ui_html(
-        openapi_url=app.openapi_url,
+        openapi_url="/backend" + app.openapi_url,
         title=app.title + " - Swagger UI",
         oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
         swagger_js_url="https://unpkg.com/swagger-ui-dist@5.9.0/swagger-ui-bundle.js",
